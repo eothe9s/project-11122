0fa800a4890a2487308e0011511732a2fbfabf34
eothe9s <hahahajeong92@gmail.com>
2026-08-10 19:59:13 +0900

    Add files via upload

---
+++ sw-v65.js
@@ -0,0 +1,5 @@
+const CACHE='project-11122-v65-20260810-1725';
+const ASSETS=['./','./index.html?v=650','./styles-v65.css?v=650','./app-v65.js?v=650','./manifest-v65.webmanifest?v=650','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html?v=620'))))});
